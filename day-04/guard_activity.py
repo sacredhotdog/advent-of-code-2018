cc9c78299f108913f940d7fdac5202de9a4607dd
sacredhotdog <dev@sacredhotdog.com>
2018-12-22 13:16:08 -0500

    guard_activity refactoring

--- day-04/guard_activity.py
+++ day-04/guard_activity.py
@@ -1,10 +1,13 @@
 from log_parser import LogParser
 from scipy import stats
 
-TIME_ASLEEP_KEY = "time_asleep"
-GUARD_ID_KEY = "guard_id"
-TIMES_KEY = "times"
-MINUTES_ASLEEP_KEY = "minutes_asleep"
+
+_TIME_ASLEEP_KEY = "time_asleep"
+_GUARD_ID_KEY = "guard_id"
+_TIMES_KEY = "times"
+_MINUTES_ASLEEP_KEY = "minutes_asleep"
+_MOST_COMMON_MINUTE_KEY = "most_common_minute"
+_COUNT_KEY = "count"
 
 
 class GuardActivity:
@@ -12,58 +15,58 @@ class GuardActivity:
     def __init__(self):
         self._log_entries = None
         self._guard_data = {}
-        self._sleepist_guard = {GUARD_ID_KEY: 0, TIME_ASLEEP_KEY: 0}
+        self._sleepist_guard = {_GUARD_ID_KEY: 0, _TIME_ASLEEP_KEY: 0}
 
-    def event_key(self, event):
+    def add_events(self, log_entries):
+        self._log_entries = sorted(log_entries, key=self.event_key)
+        self.process_events()
+
+    @staticmethod
+    def event_key(event):
         datetime_key = event.event_datetime.strftime("%Y%m%d%H%M")
 
         return datetime_key
 
-    def add_events(self, log_entries):
-        self._log_entries = sorted(log_entries, key=self.event_key)
-
-        self.process_events()
-
     def process_events(self):
         if self._log_entries is not None:
-            guard_id = 0
-            falls_asleep_time = None
-            wakes_up_time = None
+            current_guard_id = 0
+            fell_asleep_at = None
 
             for log_entry in self._log_entries:
                 if log_entry.is_start_shift_event():
-                    guard_id = log_entry.guard_id
+                    current_guard_id = log_entry.guard_id
 
-                    if self._guard_data.get(guard_id) is None:
-                        self._guard_data[guard_id] = {MINUTES_ASLEEP_KEY: 0, TIMES_KEY: []}
+                    if self._guard_data.get(current_guard_id) is None:
+                        self._guard_data[current_guard_id] = {_MINUTES_ASLEEP_KEY: 0, _TIMES_KEY: []}
                 elif log_entry.is_falls_asleep_event():
-                    falls_asleep_time = log_entry.event_datetime
+                    fell_asleep_at = log_entry.event_datetime
                 elif log_entry.is_wakes_up_event():
-                    wakes_up_time = log_entry.event_datetime
-                    minutes_aleep = int((wakes_up_time - falls_asleep_time).total_seconds()) / 60
+                    woke_up_at = log_entry.event_datetime
+                    minutes_aleep = self.convert_seconds_to_minutes(fell_asleep_at, woke_up_at)
 
-                    self._guard_data[guard_id][MINUTES_ASLEEP_KEY] += minutes_aleep
-                    self._guard_data[guard_id][TIMES_KEY].append([falls_asleep_time, wakes_up_time])
+                    self._guard_data[current_guard_id][_MINUTES_ASLEEP_KEY] += minutes_aleep
+                    self._guard_data[current_guard_id][_TIMES_KEY].append([fell_asleep_at, woke_up_at])
 
-                    if self._guard_data[guard_id][MINUTES_ASLEEP_KEY] > self._sleepist_guard[TIME_ASLEEP_KEY]:
-                        self._sleepist_guard[GUARD_ID_KEY] = guard_id
-                        self._sleepist_guard[TIME_ASLEEP_KEY] = self._guard_data[guard_id][MINUTES_ASLEEP_KEY]
+                    if self._guard_data[current_guard_id][_MINUTES_ASLEEP_KEY] > self._sleepist_guard[_TIME_ASLEEP_KEY]:
+                        self._sleepist_guard[_GUARD_ID_KEY] = current_guard_id
+                        self._sleepist_guard[_TIME_ASLEEP_KEY] = self._guard_data[current_guard_id][_MINUTES_ASLEEP_KEY]
 
-                    falls_asleep_time = None
-                    wakes_up_time = None
+    @staticmethod
+    def convert_seconds_to_minutes(fell_asleep_at, woke_up_at):
+        return (woke_up_at - fell_asleep_at).total_seconds() / 60
 
     def get_total_time_asleep(self, guard_id):
         if self._guard_data.get(guard_id) is not None:
-            return self._guard_data[guard_id][MINUTES_ASLEEP_KEY]
+            return self._guard_data[guard_id][_MINUTES_ASLEEP_KEY]
 
     def get_ordered_events(self):
         return self._log_entries
 
     def get_sleepiest_guard(self):
-        return self._sleepist_guard[GUARD_ID_KEY]
+        return self._sleepist_guard[_GUARD_ID_KEY]
 
     def get_times_for_guard(self, guard_id):
-        return self._guard_data[guard_id][TIMES_KEY]
+        return self._guard_data[guard_id][_TIMES_KEY]
 
     def get_most_common_time_asleep(self, guard_id):
         times = self.get_times_for_guard(guard_id)
@@ -81,21 +84,21 @@ class GuardActivity:
         return mode[0], mode[1]
 
     def get_most_common_time_asleep_for_all_guards(self):
-        result = {"guard_id": 0, "most_common_minute": 0, "count": 0}
+        result = {_GUARD_ID_KEY: 0, _MOST_COMMON_MINUTE_KEY: 0, _COUNT_KEY: 0}
 
         for guard_key, guard_value in self._guard_data.items():
-            minute, count = self.get_most_common_time_asleep(guard_key)
+            most_common_minute, no_of_occurrences = self.get_most_common_time_asleep(guard_key)
 
-            if count > result["count"]:
-                result["guard_id"] = guard_key
-                result["most_common_minute"] = minute
-                result["count"] = count
+            if no_of_occurrences > result[_COUNT_KEY]:
+                result[_GUARD_ID_KEY] = guard_key
+                result[_MOST_COMMON_MINUTE_KEY] = most_common_minute
+                result[_COUNT_KEY] = no_of_occurrences
 
-        return result["guard_id"], result["most_common_minute"], result["count"]
+        return result[_GUARD_ID_KEY], result[_MOST_COMMON_MINUTE_KEY], result[_COUNT_KEY]
 
 
 if __name__ == "__main__":
-    log_entries = []
+    all_log_entries = []
 
     with open("guard-log.txt") as file_object:
         file_content = file_object.read()
@@ -103,10 +106,10 @@ if __name__ == "__main__":
 
         for line in file_content.split("\n"):
             if line:
-                log_entries.append(log_parser.parse(line))
+                all_log_entries.append(log_parser.parse(line))
 
     guard_activity = GuardActivity()
-    guard_activity.add_events(log_entries)
+    guard_activity.add_events(all_log_entries)
     sleepiest_guard_id = guard_activity.get_sleepiest_guard()
 
     time_asleep = guard_activity.get_total_time_asleep(sleepiest_guard_id)
@@ -117,7 +120,8 @@ if __name__ == "__main__":
 
     print(" -> Part 1 answer: guard ID x most common minute = " + str(sleepiest_guard_id * most_common_minute_asleep))
 
-    guard_id, minute, count = guard_activity.get_most_common_time_asleep_for_all_guards()
-    print("Guard " + str(guard_id) + " was asleep " + str(count) + " times at minute " + str(minute))
+    sleepiest_guard_id, most_common_minute_asleep, count = guard_activity.get_most_common_time_asleep_for_all_guards()
+    print("Guard " + str(sleepiest_guard_id) + " was asleep " + str(count) + " times at minute " + str(
+        most_common_minute_asleep))
 
-    print(" -> Part 2 answer: guard ID x most common minute = " + str(guard_id * minute))
+    print(" -> Part 2 answer: guard ID x most common minute = " + str(sleepiest_guard_id * most_common_minute_asleep))
