2c9e1501b775349ccf542ef9aeb78a6203f5e6cb
sacredhotdog <dev@sacredhotdog.com>
2018-12-22 12:52:42 -0500

    log_entry housekeeping

--- day-04/log_parser.py
+++ day-04/log_parser.py
@@ -25,27 +25,41 @@ class LogParser:
                                               "(?P<event>[a-z ]+)")
 
     def parse(self, log_input):
+        log_entry = None
+
         if log_input is not None:
-            if log_input.find("Guard") > 0:
+            if self.is_start_of_shift_log_entry(log_input):
                 matches = self._guard_regex.match(log_input)
 
                 if matches:
-                    log_entry = LogEntry()
-                    log_entry.event_datetime = datetime(int(matches[_YEAR]), int(matches[_MONTH]), int(matches[_DAY]),
-                                                        int(matches[_HOUR]), int(matches[_MINUTE]))
-                    log_entry.guard_id = int(matches[_GUARD_ID])
-                    log_entry.event = matches[_EVENT]
-
-                    return log_entry
+                    log_entry = self.create_start_of_shift_log_entry(matches)
             else:
                 matches = self._other_events_regex.match(log_input)
 
             if matches:
-                log_entry = LogEntry()
-                log_entry.event_datetime = datetime(int(matches[_YEAR]), int(matches[_MONTH]), int(matches[_DAY]),
-                                                    int(matches[_HOUR]), int(matches[_MINUTE]))
-                log_entry.event = matches[_EVENT]
+                log_entry = self.create_log_entry(matches)
 
-                return log_entry
+        return log_entry
 
-        return None
+    @staticmethod
+    def is_start_of_shift_log_entry(log_input):
+        return log_input.find("Guard") > 0
+
+    @staticmethod
+    def create_start_of_shift_log_entry(matches):
+        log_entry = LogEntry()
+        log_entry.event_datetime = datetime(int(matches[_YEAR]), int(matches[_MONTH]), int(matches[_DAY]),
+                                            int(matches[_HOUR]), int(matches[_MINUTE]))
+        log_entry.guard_id = int(matches[_GUARD_ID])
+        log_entry.event = matches[_EVENT]
+
+        return log_entry
+
+    @staticmethod
+    def create_log_entry(matches):
+        log_entry = LogEntry()
+        log_entry.event_datetime = datetime(int(matches[_YEAR]), int(matches[_MONTH]), int(matches[_DAY]),
+                                            int(matches[_HOUR]), int(matches[_MINUTE]))
+        log_entry.event = matches[_EVENT]
+
+        return log_entry
